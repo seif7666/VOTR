83b1644df62205cb2a059fa700f192438206c6d4
Seif Wael <seifeldin7666@gmail.com>
2022-12-26 20:45:47 +0200

    Bug

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -40,14 +40,14 @@ class DistributedSampler(_DistributedSampler):
 def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None, workers=4,
                      logger=None, training=True, merge_all_iters_to_one_epoch=False, total_epochs=0):
 
-    dataset = __all__[dataset_cfg.DATASET](
+    dataset = KittiDataset(
         dataset_cfg=dataset_cfg,
         class_names=class_names,
         root_path=root_path,
         training=training,
         logger=logger,
     )
-    print(f'Root path is {root_path}')
+    # print(f'Root path is {root_path}')
 
     if merge_all_iters_to_one_epoch:
         assert hasattr(dataset, 'merge_all_iters_to_one_epoch')
